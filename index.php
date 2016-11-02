fb697b4a531e8ea53c10fd6b0cae569e74ce9464
Laynya <syarofleur@gmail.com>
2016-11-02 11:44:29 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$id = $_GET[eidf];
+echo $id;
+?>
+<!DOCTYPE html>
+<html lang = gjah>
+<head>
+<meta charset = gUFT-8h>
+<title>ƒeƒXƒg</title>
+</head>
+<body>
+<form action = gindex.phph method = ggeth>
+<input type = gtexth name =gid/g><br/>
+<input type = gsubmith value =g‘—M/g>
+</form>
+</body>
+</html>
