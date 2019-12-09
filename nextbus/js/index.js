687a0816bf3050cc917cf4fa6fe8648600db039c
Laynya <syarofleur@gmail.com>
2019-12-09 16:16:06 +0900

    first

--- nextbus/js/index.js
+++ nextbus/js/index.js
@@ -12,35 +12,40 @@ function init() {
   title.id = "title";
   title.innerHTML = "Test";
 
-  var countdown = document.createElement('div');
-  countdown.id = "countdown";
-  countdown.innerHTML = "";
-
   let objBody = document.getElementsByTagName("body").item(0);
   objBody.appendChild(title);
 
-  navigator.geolocation.getCurrentPosition(test2);
+  //ユーザーの現在の位置情報を取得
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 }
 
-function opening(){
-    let objBody = document.getElementsByTagName("body").item(0);
-    objBody.appendChild(title);
+/***** ユーザーの現在の位置情報を取得 *****/
+function successCallback(position) {
+  var gl_text = "緯度：" + position.coords.latitude + "<br>";
+    gl_text += "経度：" + position.coords.longitude + "<br>";
+    gl_text += "高度：" + position.coords.altitude + "<br>";
+    gl_text += "緯度・経度の誤差：" + position.coords.accuracy + "<br>";
+    gl_text += "高度の誤差：" + position.coords.altitudeAccuracy + "<br>";
+    gl_text += "方角：" + position.coords.heading + "<br>";
+    gl_text += "速度：" + position.coords.speed + "<br>";
+  document.getElementById("show_result").innerHTML = gl_text;
 }
 
-function test2(position) {
-
-    var geo_text = "緯度:" + position.coords.latitude + "\n";
-    geo_text += "経度:" + position.coords.longitude + "\n";
-    geo_text += "高度:" + position.coords.altitude + "\n";
-    geo_text += "位置精度:" + position.coords.accuracy + "\n";
-    geo_text += "高度精度:" + position.coords.altitudeAccuracy  + "\n";
-    geo_text += "移動方向:" + position.coords.heading + "\n";
-    geo_text += "速度:" + position.coords.speed + "\n";
-
-    var date = new Date(position.timestamp);
-
-    geo_text += "取得時刻:" + date.toLocaleString() + "\n";
-
-    alert(geo_text);
-
+/***** 位置情報が取得できない場合 *****/
+function errorCallback(error) {
+  var err_msg = "";
+  switch(error.code)
+  {
+    case 1:
+      err_msg = "位置情報の利用が許可されていません";
+      break;
+    case 2:
+      err_msg = "デバイスの位置が判定できません";
+      break;
+    case 3:
+      err_msg = "タイムアウトしました";
+      break;
+  }
+  document.getElementById("show_result").innerHTML = err_msg;
+  //デバッグ用→　document.getElementById("show_result").innerHTML = error.message;
 }
