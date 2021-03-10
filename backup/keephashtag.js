99b2666ef1e9d215ac77ce3529033b1d79a8e502
Laynya <syarofleur@gmail.com>
2021-03-10 19:37:49 +0900

    commit

---
+++ backup/keephashtag.js
@@ -0,0 +1,35 @@
+run();
+function run() {
+    let tweetTextArea = document.querySelector(".compose-content .js-compose-text");
+    if (tweetTextArea == null) {
+        setTimeout(run, 1500);
+        console.log("null");
+        return;
+    }
+    let hashTags = [];
+    let tweetObserver = new MutationObserver(function (){
+        if (tweetTextArea.disabled) {
+            hashTags = [];
+            let tweetedHashTags = tweetTextArea.value.match(/[\n 　][#＃][Ａ-Ｚａ-ｚA-Za-z一-鿆0-9０-９ぁ-ヶｦ-ﾟー_]+/g);
+            if (tweetedHashTags) {
+                for (let i = 0; i < tweetedHashTags.length; i++) {
+                    tweetedHashTags[i] = tweetedHashTags[i].substr(1);
+                }
+                hashTags = tweetedHashTags;
+            }
+        }
+        else {
+            if (hashTags.length !== 0) {
+                tweetTextArea.value = " " + hashTags.join(" ");
+            }
+            tweetTextArea.selectionStart = 0;
+            tweetTextArea.selectionEnd = 0;
+            tweetTextArea.dispatchEvent(new Event('change'));
+        }
+		console.log(hashTags);
+    });
+    tweetObserver.observe(tweetTextArea, {
+        "attributes": true,
+        "attributeFilter": ["disabled"]
+    });
+}
